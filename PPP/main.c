685cd677c153cd270224fda99895a3131708cc75
Olga Kosyura <olyakosyura@gmail.com>
2015-11-11 19:37:32 +0300

    New coursework. V0 Only plan of work

---
+++ PPP/main.c
@@ -0,0 +1,12 @@
+#include "print.h"
+int main (void)
+{
+	print_graph ();
+	massiv 1[5][5];
+	massiv 2[5][5];
+	massiv 3[5][5];
+	massiv 4[5][5];
+	massiv 5[5][5];
+	
+}
+
---
+++ PPP/print.h
@@ -0,0 +1,3 @@
+void print_graph(void);
+
+
