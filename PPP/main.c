2523f63be634429428292150f1a1b137dce8c22c
Olga Kosyura <olyakosyura@gmail.com>
2015-11-11 20:32:23 +0300

    v0.1

--- PPP/main.c
+++ PPP/main.c
@@ -2,11 +2,21 @@
 int main (void)
 {
 	print_graph ();
-	massiv 1[5][5];
-	massiv 2[5][5];
-	massiv 3[5][5];
-	massiv 4[5][5];
-	massiv 5[5][5];
+	transport();
 	
+}
+
+int transport(void)
+{
+	int ssize=4;
+	
+	int dsize=3;
+	printf("\n Ssize = %d; Dsize = %d", ssize, dsize);
+	int* destination[dsize];
+	for(int i=0; i<3; i++){destination[i]=20;}	
+	int* sourses[ssize] = (10,20,10,20);
+	int* prices[ssize][dsize] = (1,2,3,4;2,3,4,5;3,4,5,6);
+
+	printf(destination);
 }
 
